be0700b2a66336bcaf8cdfc952ea363c34bc7867
agusramirez126w-creator <agusramirez126w@gmail.com>
2026-05-28 23:26:13 -0300

    Update analisis.py

--- datos/scripts/analisis.py
+++ datos/scripts/analisis.py
@@ -1,1 +1,17 @@
+ventas = [100, 200, 300, 150]
 
+total = sum(ventas)
+promedio = total / len(ventas)
+maximo = max(ventas)
+
+print("Ventas totales:", total)
+print("Promedio:", promedio)
+print("Venta máxima:", maximo)
+
+archivo = open("../resultados/resultados.txt", "w")
+
+archivo.write(f"Ventas totales: {total}\n")
+archivo.write(f"Promedio: {promedio}\n")
+archivo.write(f"Venta máxima: {maximo}")
+
+archivo.close()
